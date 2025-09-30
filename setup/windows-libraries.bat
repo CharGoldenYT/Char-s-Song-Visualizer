a3284a8bdc2d4a0d9ddf9adb52dae418b140da79
Char <73309364+CharGoldenYT@users.noreply.github.com>
2025-09-30 03:21:52 -0400

    setup stuffs

---
+++ setup/windows-libraries.bat
@@ -0,0 +1,3 @@
+haxelib install flixel 5.5.0 && haxelib set flixel 5.5.0
+haxelib install flixel-addons 3.2.0 && haxelib set flixel-addons 3.2.0
+haxelib install flixel-ui 2.5.0 && haxelib set flixel-ui 2.5.0
