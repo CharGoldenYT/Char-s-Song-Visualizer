a7e0c523cf8266a6fd0d09bef20244773f090f7c
Char <73309364+CharGoldenYT@users.noreply.github.com>
2025-10-05 14:59:02 -0400

    fix the library thingies

--- setup/windows-libraries.bat
+++ setup/windows-libraries.bat
@@ -1,3 +1,5 @@
+haxelib install openfl
+haxelib install lime 8.2.2 && haxelib set lime 8.2.2
 haxelib install flixel 5.5.0 && haxelib set flixel 5.5.0
 haxelib install flixel-addons 3.2.0 && haxelib set flixel-addons 3.2.0
 haxelib install flixel-ui 2.5.0 && haxelib set flixel-ui 2.5.0
