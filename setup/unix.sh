8f1de8a46b2cf0163fc9bc9e610d2102d4f16bf8
Char <73309364+CharGoldenYT@users.noreply.github.com>
2025-10-05 15:05:04 -0400

    forgot libraries

--- setup/unix.sh
+++ setup/unix.sh
@@ -3,3 +3,4 @@ haxelib install lime 8.2.2 && haxelib set lime 8.2.2
 haxelib install flixel 5.5.0 && haxelib set flixel 5.5.0
 haxelib install flixel-addons 3.2.0 && haxelib set flixel-addons 3.2.0
 haxelib install flixel-ui 2.5.0 && haxelib set flixel-ui 2.5.0
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
